e05dc628cc397479528dc1c579c8c509c2a0c332
tnyagwaya <thomas.nyagwaya@econet.co.zw>
2023-09-20 00:12:31 +0200

    .

---
+++ src/main/java/zw/co/reikan/nanoloansweb/NanoLoansWebApplication.java
@@ -0,0 +1,13 @@
+package zw.co.reikan.nanoloansweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanoLoansWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanoLoansWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zw/co/reikan/nanoloansweb/NanoLoansWebApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.reikan.nanoloansweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanoLoansWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
