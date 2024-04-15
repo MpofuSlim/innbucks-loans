c7321f2ba0051b39db199747e6b0698c43882834
Thomas Nyagwaya <tnyagwaya@bluefintechnology.co.zw>
2024-04-15 14:12:11 +0200

    next of kin

--- loans-core/src/main/java/zw/co/reikan/loans/core/loan/RelationshipType.java
+++ loans-core/src/main/java/zw/co/reikan/loans/core/loan/RelationshipType.java
@@ -1,20 +1,35 @@
 package zw.co.reikan.loans.core.loan;
 
 public enum RelationshipType {
-    SPOUSE("Spouse"),
-    PARTNER("Partner"),
-    PARENT("Parent"),
-    CHILD("Child"),
-    SIBLING("Sibling"),
-    FRIEND("Friend"),
-    COLLEAGUE("Colleague"),
-    RELATIVE("Relative"),
-    OTHER("Other");
+
+    SIBLING("Sibling", "B"),
+
+    DAUGHTER("Daughter", "DA"),
+    SPOUSE("Spouse", "S"),
+    DAUGHTER_IN_LAW("Daughter-in-law", "DI"),
+    FATHER("Father", "F"),
+    FATHER_IN_LAW("Father-in-law", "FI"),
+    GUARDIAN("Guardian", "G"),
+    MOTHER("Mother", "M"),
+    MOTHER_IN_LAW("Mother-in-law", "MI"),
+
+    NEPHEW("Nephew", "NE"),
+
+    SISTER_IN_LAW("Sister-in-law", "SI"),
+
+    SON("Son", "S");
+
 
     private final String displayName;
+    private final String code;
 
-    RelationshipType(String displayName) {
+    RelationshipType(String displayName, String code) {
         this.displayName = displayName;
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
     }
 
     public String getDisplayName() {
