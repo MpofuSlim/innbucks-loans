38f2d12a89193a14f6fd8594c3efa083f0157057
Thomas Nyagwaya <tnyagwaya@bluefintechnology.co.zw>
2024-07-30 20:09:21 +0200

    add skm motorcycles

--- loans-core/src/main/java/zw/co/reikan/loans/core/loan/Merchant.java
+++ loans-core/src/main/java/zw/co/reikan/loans/core/loan/Merchant.java
@@ -6,6 +6,7 @@ import static zw.co.reikan.loans.core.loan.DisbursementType.MERCHANT_MOBILE_WALL
 public enum Merchant {
     INNBUCKS(CUSTOMER_MOBILE_WALLET),
     HOUSE_AND_HOME(MERCHANT_MOBILE_WALLET),
+    SKM_MOTORCYCLES(MERCHANT_MOBILE_WALLET),
     CAPRI(MERCHANT_MOBILE_WALLET);
 
     final DisbursementType disbursementType;
